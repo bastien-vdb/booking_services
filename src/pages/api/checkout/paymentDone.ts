1cb9f3762226e1c34a2ad6bb98df517a5a71b4e0
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-07-16 18:47:19 +0200

    paymentDone route setup2

--- src/pages/api/checkout/paymentDone.ts
+++ src/pages/api/checkout/paymentDone.ts
@@ -8,10 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
 });
 
-export default async function webhookHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function webhookHandler(req: NextApiRequest, res: NextApiResponse) {
   const event = req.body;
 
   if (!process.env.STRIPE_WEBHOOK_SECRET) throw new Error("Stripe webhook secret key is not defined");
@@ -20,11 +17,7 @@ export default async function webhookHandler(
     // Verify the webhook event using your webhook signing secret
     const signature = req.headers["stripe-signature"];
     if (signature === undefined) throw new Error("Stripe signature is not defined");
-    const webhookEvent = stripe.webhooks.constructEvent(
-      req.body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
+    const webhookEvent = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
 
     // Handle the specific event type
     switch (webhookEvent.type) {
@@ -33,6 +26,14 @@ export default async function webhookHandler(
         // Payment was successful, you can retrieve relevant details from the session object
         console.log("Payment completed:", session);
         // Perform any necessary actions (e.g., update booking status, send confirmation email, etc.)
+        break;
+      case "checkout.session.async_payment_succeeded":
+        const checkoutSessionAsyncPaymentSucceeded = webhookEvent.data.object;
+        // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+        break;
+      case "checkout.session.completed":
+        const checkoutSessionCompleted = event.data.object;
+        // Then define and call a function to handle the event checkout.session.completed
         break;
       default:
         // Handle other webhook event types if needed
