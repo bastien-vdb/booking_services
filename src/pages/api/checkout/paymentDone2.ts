956a8a327ea793e6d346db2d8e4d2d702bd90f72
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-07-16 20:47:12 +0200

    paymentDone route setup7

--- src/pages/api/checkout/paymentDone2.ts
+++ src/pages/api/checkout/paymentDone2.ts
@@ -2,5 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Stripe } from "stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!process.env.STRIPE_SECRET_KEY) throw new Error("Stripe secret key is not defined");
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-15",
+  });
+
+  const event = req.body;
   res.status(200).json("La route fonctionne");
 }
