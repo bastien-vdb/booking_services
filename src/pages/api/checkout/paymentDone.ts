17856d97e26a93918ab760c31f9f86dcd49a0805
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-07-16 20:41:57 +0200

    paymentDone route setup5

--- src/pages/api/checkout/paymentDone.ts
+++ src/pages/api/checkout/paymentDone.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Stripe } from "stripe";
 
-// Initialize Stripe with your secret key
-if (!process.env.STRIPE_SECRET_KEY) throw new Error("Stripe secret key is not defined");
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Initialize Stripe with your secret key
+  if (!process.env.STRIPE_SECRET_KEY) throw new Error("Stripe secret key is not defined");
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-});
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-15",
+  });
 
-export default async function webhookHandler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json('La route fonctionne');
+  res.status(200).json("La route fonctionne");
   const event = req.body;
 
   if (!process.env.STRIPE_WEBHOOK_SECRET) throw new Error("Stripe webhook secret key is not defined");
