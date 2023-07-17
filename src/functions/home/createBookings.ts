6046d0a1ca6bd37b22b2244b2835068933238aed
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-07-17 13:41:29 +0200

    stripe flow improvement6

--- src/functions/home/createBookings.ts
+++ src/functions/home/createBookings.ts
@@ -1,13 +1,13 @@
 import { Service } from "@prisma/client";
 import { prisma } from "@/db/prisma";
 
-async function createBookings(slot: Date, serviceId: Service["id"] | null | undefined, stripePriceId: Service["stripePriceId"] | null | undefined, userId: string | undefined): Promise<boolean> {
+async function createBookings(slot: Date, serviceId: Service["id"] | null | undefined, stripePriceId: Service["stripePriceId"] | null | undefined, userId: string | undefined): Promise<void> {
   const selectedTime = new Date(slot);
   const startTime = new Date(selectedTime);
   const endTime = new Date(selectedTime.getTime() + 1 * 60 * 60 * 1000);
 
   try {
-    const newBooking = await prisma.booking.create({
+    await prisma.booking.create({
       data: {
         date: startTime,
         startTime,
